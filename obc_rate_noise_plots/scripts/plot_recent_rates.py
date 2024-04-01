594c0cbec416fa6732246f55718869bef9466945
Javier Gonzalez <javierggt@yahoo.com>
2024-04-01 13:20:33 -0400

    plot_recent_rates.py: encapsulate script in main function

--- obc_rate_noise_plots/scripts/plot_recent_rates.py
+++ obc_rate_noise_plots/scripts/plot_recent_rates.py
@@ -31,69 +31,75 @@ def get_options():
     (opt, args) = parser.parse_args()
     return (opt, args)
 
-opt, args = get_options()
+
+def main():
+    opt, args = get_options()
 
 
-tstop = DateTime(opt.stop).secs  # if opt.stop is None then this returns current time
-if opt.start is None:
-    tstart = tstop - opt.duration
-else:
-    tstart = DateTime(opt.start).secs
+    tstop = DateTime(opt.stop).secs  # if opt.stop is None then this returns current time
+    if opt.start is None:
+        tstart = tstop - opt.duration
+    else:
+        tstart = DateTime(opt.start).secs
 
-if 'msids' not in globals():
-    dwells = events.dwells.filter(tstart, tstop)
-    tstop = np.min([dwells[len(dwells) - 1].tstop, tstop])
-    msids = fetch.MSIDset(['aorate1', 'aorate2', 'aorate3'],
-                          tstart, tstop, filter_bad=True)
+    if 'msids' not in globals():
+        dwells = events.dwells.filter(tstart, tstop)
+        tstop = np.min([dwells[len(dwells) - 1].tstop, tstop])
+        msids = fetch.MSIDset(['aorate1', 'aorate2', 'aorate3'],
+                            tstart, tstop, filter_bad=True)
 
-# Filter to times within Kalman interval with no momentum dumps or SIM moves nearby
-if 'times' not in globals():
-    print('Calculating and final filtering')
-    events.dwells.interval_pad = (-1000, -1000)
-    events.tsc_moves.interval_pad = (1000, 1000)
-    events.dumps.interval_pad = (1000, 1000)
-    for msid in msids:
-        msids[msid].select_intervals(events.dwells)
-        msids[msid].remove_intervals(events.tsc_moves)
-        msids[msid].remove_intervals(events.dumps)
-    roll_rates = msids['aorate1'].vals * 206264
-    pitch_rates = msids['aorate2'].vals * 206264
-    yaw_rates = msids['aorate3'].vals * 206264
-    times = msids['aorate1'].times
+    # Filter to times within Kalman interval with no momentum dumps or SIM moves nearby
+    if 'times' not in globals():
+        print('Calculating and final filtering')
+        events.dwells.interval_pad = (-1000, -1000)
+        events.tsc_moves.interval_pad = (1000, 1000)
+        events.dumps.interval_pad = (1000, 1000)
+        for msid in msids:
+            msids[msid].select_intervals(events.dwells)
+            msids[msid].remove_intervals(events.tsc_moves)
+            msids[msid].remove_intervals(events.dumps)
+        roll_rates = msids['aorate1'].vals * 206264
+        pitch_rates = msids['aorate2'].vals * 206264
+        yaw_rates = msids['aorate3'].vals * 206264
+        times = msids['aorate1'].times
 
-time_range = DateTime(tstart).date[:8] + ' to ' + DateTime(tstop).date[:8]
-if 1:
-    plt.figure(1, figsize=(4, 2.75))
-    bins = np.linspace(-4, 4, 60)
-    for label, rates in (('roll', roll_rates),
-                         ('pitch', pitch_rates),
-                         ('yaw', yaw_rates)):
-        print('Making', label, 'histograms')
-        plt.clf()
-        plt.hist(rates, bins=bins, log=True, color='blue')
-        plt.title(label.capitalize() + ' rates {0}'.format(time_range))
-        plt.xlabel('Rate (arcsec/sec)')
-        plt.ylabel('Frequency')
-        plt.tight_layout()
-        #plt.subplots_adjust(left=0.16, bottom=0.14)
-        plt.grid(linestyle='--')
-        plt.show()
-        plt.savefig(os.path.join(opt.outdir, label + '_hist_%s.png' % opt.root))
+    time_range = DateTime(tstart).date[:8] + ' to ' + DateTime(tstop).date[:8]
+    if 1:
+        plt.figure(1, figsize=(4, 2.75))
+        bins = np.linspace(-4, 4, 60)
+        for label, rates in (('roll', roll_rates),
+                            ('pitch', pitch_rates),
+                            ('yaw', yaw_rates)):
+            print('Making', label, 'histograms')
+            plt.clf()
+            plt.hist(rates, bins=bins, log=True, color='blue')
+            plt.title(label.capitalize() + ' rates {0}'.format(time_range))
+            plt.xlabel('Rate (arcsec/sec)')
+            plt.ylabel('Frequency')
+            plt.tight_layout()
+            #plt.subplots_adjust(left=0.16, bottom=0.14)
+            plt.grid(linestyle='--')
+            plt.show()
+            plt.savefig(os.path.join(opt.outdir, label + '_hist_%s.png' % opt.root))
 
-if 1:
-    plt.figure(2, figsize=(8, 2.75))
-    for label, rates in (('roll', roll_rates),
-                         ('pitch', pitch_rates),
-                         ('yaw', yaw_rates)):
-        print('Making', label, 'time plots')
-        plt.clf()
-        plot_cxctime(times, rates, '.', markersize=2, mew=0, color='blue', alpha=.20)
-        plt.ylim(-4, 4)
-        plt.yticks(np.arange(-4, 5))
-        plt.title(label.capitalize() + ' rates {0}'.format(time_range))
-        plt.ylabel('Rate (arcsec/sec)')
-        ax = plt.gca()
-        ax.set_axisbelow(False)
-        plt.grid(linestyle='--')
-        plt.show()
-        plt.savefig(os.path.join(opt.outdir, label + '_time_%s.png' % opt.root))
+    if 1:
+        plt.figure(2, figsize=(8, 2.75))
+        for label, rates in (('roll', roll_rates),
+                            ('pitch', pitch_rates),
+                            ('yaw', yaw_rates)):
+            print('Making', label, 'time plots')
+            plt.clf()
+            plot_cxctime(times, rates, '.', markersize=2, mew=0, color='blue', alpha=.20)
+            plt.ylim(-4, 4)
+            plt.yticks(np.arange(-4, 5))
+            plt.title(label.capitalize() + ' rates {0}'.format(time_range))
+            plt.ylabel('Rate (arcsec/sec)')
+            ax = plt.gca()
+            ax.set_axisbelow(False)
+            plt.grid(linestyle='--')
+            plt.show()
+            plt.savefig(os.path.join(opt.outdir, label + '_time_%s.png' % opt.root))
+
+
+if __name__ == '__main__':
+    main()
